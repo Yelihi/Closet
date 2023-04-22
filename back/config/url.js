b31003e1b0768ff8d84edb0365c0f1b3c57c24e1
Yelihi <Yelihi19@gmail.com>
2023-04-22 23:57:03 +0900

    fixed: export

--- back/config/url.js
+++ back/config/url.js
@@ -1,2 +1,5 @@
-export const frontUrl = "http://43.200.191.213/";
-export const backUrl = "http://15.164.228.34";
+const frontUrl = "http://43.200.191.213/";
+const backUrl = "http://15.164.228.34";
+
+module.exports = frontUrl;
+module.exports = backUrl;
