a819197b9d73c6ec1c4680830646ec00ce8ad37d
Yelihi <Yelihi19@gmail.com>
2023-04-24 09:53:57 +0900

    Fixed: filename location

--- back/routes/post.js
+++ back/routes/post.js
@@ -63,7 +63,7 @@ router.post("/images", isLoggedIn, upload.single("image"), async (req, res, next
   // POST /post/images 파일 한개씩 업로드
   console.log(req.file);
   try {
-    const filename = path.resolve(__dirname, "..", req.file.path);
+    const filename = req.file.location;
     console.log("filename", filename);
     const request = {
       image: { content: fs.readFileSync(filename) },
