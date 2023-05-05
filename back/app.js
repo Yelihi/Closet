1c65beb2d6c21b1ac975213eaa586a94f53f1f21
Yelihi <Yelihi19@gmail.com>
2023-05-06 01:09:46 +0900

    test

--- back/app.js
+++ back/app.js
@@ -69,7 +69,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: true,
-      domain: ".closet-online.com",
+      domain: process.env.NODE_ENV === "production" && ".closet-online.com",
     },
   })
 );
