24d53e7a7287d82f3f816a73d9a56be61e42bc58
Yelihi <Yelihi19@gmail.com>
2023-09-12 22:57:51 +0900

    feat: msw handler setting & userLogin test setting

--- front/__mocks__/handlers.js
+++ front/__mocks__/handlers.js
@@ -1,3 +1,4 @@
 import { rest } from 'msw';
+import userLoginHandler from '../__mocks__/userLogin/handlers';
 
-export const handlers = [];
+export const handlers = [...Object.values(userLoginHandler)];
