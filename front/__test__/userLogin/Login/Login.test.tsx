a559b7c6869ec1307e08a0d72eb251696a167fcf
Yelihi <Yelihi19@gmail.com>
2023-09-13 14:24:06 +0900

    feat: test setting in Login Page

--- front/__test__/userLogin/Login/Login.test.tsx
+++ front/__test__/userLogin/Login/Login.test.tsx
@@ -1,29 +1,47 @@
-import { screen, render } from '@testing-library/react';
-import { renderWithRedux } from '../../../util/TestUtils/renderWithRedux';
+import { screen } from '@testing-library/react';
+import { renderHandler } from '../userLogin.test';
 
-import Login from '../../../components/auth/Login';
+export type DefaultValueInLogin = {
+  LoginEmail: string;
+  LoginPassword: string;
+};
 
-const toggleGoToAccount = jest.fn();
+export const defaultFillOutValueInLogin: DefaultValueInLogin = {
+  LoginEmail: 'yelihi19@gmail.com',
+  LoginPassword: 'abc123456!',
+};
 
 describe('Login Component', () => {
   it('render correctly', () => {
-    // Arrange
-    renderWithRedux(<Login toggleGotoAccount={toggleGoToAccount} />);
+    const renderElements = renderHandler.login();
 
-    const emailElement = screen.getByPlaceholderText(/email/i);
-    const passwordElement = screen.getByPlaceholderText(/password/i);
-    const signInButtonElement = screen.getByRole('button', { name: 'Sign in' });
-    const createAccountButtonElement = screen.getByRole('button', { name: 'Create account' });
-
-    // Assert
-    expect(emailElement).toBeInTheDocument();
-    expect(passwordElement).toBeInTheDocument();
-    expect(signInButtonElement).toBeInTheDocument();
-    expect(createAccountButtonElement).toBeInTheDocument();
+    for (const elem in renderElements) {
+      expect(renderElements[elem as keyof typeof renderElements]).toBeInTheDocument();
+    }
   });
 
-  it.skip('이메일 유효성 검사에 적합하지 않을 시 에러 메시지 렌더링', () => {});
-  it.skip('비밀벝호 유효성 검사에 적합하지 않을 시 에러 메시지 렌더링', () => {});
-  it.skip('유효성 검사에 적합하지 않을 시 Sign in 비활성화', () => {});
-  it.skip('유효성 검사를 통과할 시 Sign in 활성화', () => {});
+  it('이메일 유효성 검사에 적합하지 않을 시 에러 메시지 렌더링(이메일형식)', () => {
+    renderHandler.fillOutLoginInputValidExceptProps(defaultFillOutValueInLogin, 'LoginEmail', 'rock7246com');
+
+    const ErrorMessage = screen.getByText(/이메일이 올바르지 않습니다/i);
+
+    expect(ErrorMessage).toBeInTheDocument();
+  });
+  it('비밀벝호 유효성 검사에 적합하지 않을 시 에러 메시지 렌더링(8자 미만)', () => {
+    renderHandler.fillOutLoginInputValidExceptProps(defaultFillOutValueInLogin, 'LoginPassword', '12345');
+
+    const ErrorMessage = screen.getByText(/비밀번호가 올바르지 않습니다/i);
+
+    expect(ErrorMessage).toBeInTheDocument();
+  });
+  it('유효성 검사에 하나라도 적합하지 않을 시 Sign in 비활성화', () => {
+    const { LoginButton } = renderHandler.fillOutLoginInputValidExceptProps(defaultFillOutValueInLogin, 'LoginPassword', '12345');
+
+    expect(LoginButton).toBeDisabled();
+  });
+  it('유효성 검사를 통과할 시 Sign in 활성화', () => {
+    const { LoginButton } = renderHandler.fillOutLoginInputValidExceptProps(defaultFillOutValueInLogin);
+
+    expect(LoginButton).not.toBeDisabled();
+  });
 });
--- front/__test__/userLogin/SignUp/Signup.test.tsx
+++ front/__test__/userLogin/SignUp/Signup.test.tsx
@@ -1,33 +1,64 @@
-import { screen, render } from '@testing-library/react';
-import { renderWithRedux } from '../../../util/TestUtils/renderWithRedux';
+import { screen } from '@testing-library/react';
 
-import Signup from '../../../components/auth/Signup';
+import { renderHandler } from '../userLogin.test';
 
-const toggleGoToAccount = jest.fn();
+export type DefaultValueInSignUp = {
+  SignUpName: string;
+  SignUpEmail: string;
+  SignUpPassword: string;
+  SignUpCheck: string;
+};
 
-describe('Login Component', () => {
+export const defaultFillOutValueInSignUp: DefaultValueInSignUp = {
+  SignUpName: 'wonik',
+  SignUpEmail: 'yelihi19@gmail.com',
+  SignUpPassword: 'abc123456!',
+  SignUpCheck: 'abc123456!',
+};
+
+describe('SignUp Component', () => {
   it('render correctly', () => {
     // Arrange
-    renderWithRedux(<Signup toggleGotoAccount={toggleGoToAccount} />);
+    const renderElements = renderHandler.signUp();
 
-    const nameElement = screen.getByPlaceholderText(/name/i);
-    const emailElement = screen.getByPlaceholderText(/email/i);
-    const passwordElement = screen.getByPlaceholderText('Password');
-    const checkElement = screen.getByPlaceholderText(/password check/i);
-    const signInButtonElement = screen.getByRole('button', { name: 'Create account' });
-    const createAccountButtonElement = screen.getByRole('button', { name: 'back' });
-
-    // Assert
-    expect(nameElement).toBeInTheDocument();
-    expect(emailElement).toBeInTheDocument();
-    expect(passwordElement).toBeInTheDocument();
-    expect(checkElement).toBeInTheDocument();
-    expect(signInButtonElement).toBeInTheDocument();
-    expect(createAccountButtonElement).toBeInTheDocument();
+    for (const elem in renderElements) {
+      expect(renderElements[elem as keyof typeof renderElements]).toBeInTheDocument();
+    }
   });
 
-  it.skip('이메일 유효성 검사에 적합하지 않을 시 에러 메시지 렌더링', () => {});
-  it.skip('비밀벝호 유효성 검사에 적합하지 않을 시 에러 메시지 렌더링', () => {});
-  it.skip('유효성 검사에 적합하지 않을 시 Sign in 비활성화', () => {});
-  it.skip('유효성 검사를 통과할 시 Sign in 활성화', () => {});
+  it('이메일 유효성 검사에 적합하지 않을 시 에러 메시지 렌더링(이메일의 양식)', () => {
+    renderHandler.fillOutFormVaildExceptProps(defaultFillOutValueInSignUp, 'SignUpEmail', 'Rock5243com');
+
+    const ErrorMessage = screen.getByText(/이메일이 올바르지 않습니다/i);
+
+    expect(ErrorMessage).toBeInTheDocument();
+  });
+  it('비밀벝호 유효성 검사에 적합하지 않을 시 에러 메시지 렌더링(8자리 미만)', () => {
+    renderHandler.fillOutFormVaildExceptProps(defaultFillOutValueInSignUp, 'SignUpPassword', '1234');
+
+    const ErrorMessage = screen.getByText(/비밀번호가 올바르지 않습니다/i);
+
+    expect(ErrorMessage).toBeInTheDocument();
+  });
+  it('비밀번호 확인 시 입력 비밀번호와 일치하지 않을 시 에러 메시지 렌더링', () => {
+    renderHandler.fillOutFormVaildExceptProps(defaultFillOutValueInSignUp, 'SignUpCheck', '1234567');
+
+    const ErrorMessage = screen.getByText(/비밀번호가 일치하지 않습니다/i);
+
+    expect(ErrorMessage).toBeInTheDocument();
+  });
+  it('유효성 검사에 하나라도 적합하지 않을 시 SignUp 버튼 비활성화', () => {
+    renderHandler.fillOutFormVaildExceptProps(defaultFillOutValueInSignUp, 'SignUpEmail', 'Rock5243com');
+
+    const SignUpButton = screen.getByTestId('submitButton');
+
+    expect(SignUpButton).toBeDisabled();
+  });
+  it('유효성 검사를 통과할 시 Sign in 활성화', () => {
+    renderHandler.fillOutFormVaildExceptProps(defaultFillOutValueInSignUp);
+
+    const SignUpButton = screen.getByTestId('submitButton');
+
+    expect(SignUpButton).not.toBeDisabled();
+  });
 });
