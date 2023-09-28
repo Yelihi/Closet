7e8caed14cb84f6177a30595c7e5728975dd91a0
Yelihi <Yelihi19@gmail.com>
2023-09-29 02:43:54 +0900

    feat: Auth 내 MemberContext.tsx 추가

---
+++ front/components/auth/MemberContext.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import type {
+  MemberInfoProps,
+  IsValiedInfoProps,
+  PartialMemberInfoProps,
+  PartialIsValiedInfoProps,
+  SignUpInfoProps,
+  IsValiedSignUpInfoProps,
+  PartialSignUpInfoProps,
+  PartialIsValiedSignUpInfoProps,
+} from './Type';
+
+export type LoginContextType = {
+  value: MemberInfoProps;
+  setValue: (v: PartialMemberInfoProps) => void;
+  error: IsValiedInfoProps;
+  setError: (e: PartialIsValiedInfoProps) => void;
+};
+
+export type SignUpContextType = {
+  value: SignUpInfoProps;
+  setValue: (v: PartialSignUpInfoProps) => void;
+  error: IsValiedSignUpInfoProps;
+  setError: (e: PartialIsValiedSignUpInfoProps) => void;
+};
+
+export function createAuthContext<T>() {
+  return React.createContext<T | null>(null);
+}
+
+export const LoginContext = createAuthContext<LoginContextType>();
+export const SignUpContext = createAuthContext<SignUpContextType>();
