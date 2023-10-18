ed97ca61a19e007af29c9d2df8eda0afb4fa3a4a
Yelihi <Yelihi19@gmail.com>
2023-10-19 03:49:07 +0900

    feat: emptyPage 추가

---
+++ front/components/state/empty/Data.tsx
@@ -0,0 +1,23 @@
+import Router from 'next/router';
+import empty from '../../../public/AnimaionJson/emptyDataPerson.json';
+
+const defaultStyle = {
+  width: '80%',
+  height: '50%',
+};
+
+export const useLottiePropsByEmpty = {
+  OverView: {
+    options: {
+      src: empty,
+      loop: true,
+      autoplay: true,
+      style: defaultStyle,
+    },
+    Notify: '이런! 저장된 의류가 없네요!',
+    Button: '저장 페이지로',
+    Router: () => Router.push('/closet/add'),
+  },
+} as const;
+
+export type PropsByEmpty = { state: keyof typeof useLottiePropsByEmpty };
