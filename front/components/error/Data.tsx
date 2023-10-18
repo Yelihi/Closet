925112b520887cd0bbe10a0e911b8e00ef17d968
Yelihi <Yelihi19@gmail.com>
2023-10-19 03:43:49 +0900

    feat: ErrorPage 추가

---
+++ front/components/error/Data.tsx
@@ -0,0 +1,34 @@
+import Router from 'next/router';
+import error from '../../public/AnimaionJson/error.json';
+
+const defaultStyle = {
+  width: '80%',
+  height: '50%',
+};
+
+export const useLottiePropsByError = {
+  OverView: {
+    options: {
+      src: error,
+      loop: true,
+      autoplay: true,
+      style: defaultStyle,
+    },
+    Notify: '데이터를 불러오는과정에서 에러가 발생했습니다!',
+    Button: '다시 시도하기',
+    Router: () => Router.push('/closet/overview'),
+  },
+  PriceChart: {
+    options: {
+      src: error,
+      loop: true,
+      autoplay: true,
+      style: defaultStyle,
+    },
+    Notify: '데이터를 불러오는과정에서 에러가 발생했습니다!',
+    Button: '다시 시도하기',
+    Router: () => Router.push('/closet/reports/price'),
+  },
+} as const;
+
+export type PropsByError = { state: keyof typeof useLottiePropsByError };
