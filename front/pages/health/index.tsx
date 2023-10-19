2d5c273fc1f35808a2b3045d59d439da37f52da3
Yelihi <Yelihi19@gmail.com>
2023-10-20 01:44:10 +0900

    test: health -> import 삭제

--- front/pages/health/index.tsx
+++ front/pages/health/index.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import { GetServerSidePropsContext } from 'next';
-import { getServerSideProps as Login } from '../userlogin';
-import { getServerSideProps as Overview } from '../closet/overview';
-import { getServerSideProps as Store } from '../closet/store';
-import { getServerSideProps as Add } from '../closet/add';
-import { getServerSideProps as Price } from '../closet/reports/price';
 
 const Health = () => {
   return <div>dummy</div>;
@@ -15,10 +10,5 @@ export default Health;
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   context.res.setHeader('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
   context.res.end('pong');
-  Login(context);
-  Overview(context);
-  Store(context);
-  Add(context);
-  Price(context);
   return { props: {} };
 }
