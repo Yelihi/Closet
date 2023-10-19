4858b48faee809108e47c24902b26ead8db5ef61
Yelihi <Yelihi19@gmail.com>
2023-10-19 18:51:18 +0900

    fix: 모바일 환경을 탐지하기 위한 detectMobileDevice 함수 구현

--- front/util/PrimitiveUtils/string.tsx
+++ front/util/PrimitiveUtils/string.tsx
@@ -5,3 +5,12 @@ export const convertNumberToLocales = (number: number, locales: string) => {
 export const convertNumberToKRWCurrency = (number: number) => {
   return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(number);
 };
+
+export const detectMobileDevice = (userAgent: string | undefined) => {
+  if (!userAgent) return false;
+  return Boolean(
+    userAgent.match(
+      /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/i
+    )
+  );
+};
