234fc364157202d2c909ec486da4c73ab5f6940d
Yelihi <Yelihi19@gmail.com>
2023-10-28 04:22:03 +0900

    refactor: state -> error data 부분 admin 추가

--- front/components/state/error/Data.tsx
+++ front/components/state/error/Data.tsx
@@ -40,6 +40,17 @@ export const useLottiePropsByError = {
     Button: '다시 시도하기',
     Router: () => Router.push('/closet/store'),
   },
+  Admin: {
+    options: {
+      src: error,
+      loop: true,
+      autoplay: true,
+      style: defaultStyle,
+    },
+    Notify: '데이터를 불러오는과정에서 에러가 발생했습니다!',
+    Button: '다시 시도하기',
+    Router: () => Router.push('/closet/administration'),
+  },
 } as const;
 
 export type PropsByError = { state: keyof typeof useLottiePropsByError };
