c9a2bc470a9665ac75072a658dc437b2962275e7
Yelihi <Yelihi19@gmail.com>
2023-10-30 23:55:41 +0900

    refactor: color, sort page

--- front/pages/closet/reports/color/index.tsx
+++ front/pages/closet/reports/color/index.tsx
@@ -1,26 +1,10 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 import addHead from '../../../../util/addHead';
 
-import PageLayout from '../../../../components/recycle/layout/PageLayout';
-import KeepWorking from '../../../../components/recycle/KeepWorking';
+import RenderDevelopingPage from '../../../../components/state/developing/RenderDevelopingPage';
 
 const color = () => {
-  return (
-    <PageLayout>
-      <TestContainer>
-        <KeepWorking height={100} />
-      </TestContainer>
-    </PageLayout>
-  );
+  return <RenderDevelopingPage state='Develop' />;
 };
 
 export default addHead(color, 'closet', '이 페이지는 의류 색상 차트를 보여줍니다');
-
-const TestContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 800px;
-`;
--- front/pages/closet/reports/sort/index.tsx
+++ front/pages/closet/reports/sort/index.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 import addHead from '../../../../util/addHead';
 
-import PageLayout from '../../../../components/recycle/layout/PageLayout';
-import KeepWorking from '../../../../components/recycle/KeepWorking';
+import RenderDevelopingPage from '../../../../components/state/developing/RenderDevelopingPage';
 
 function sort() {
-  return (
-    <PageLayout>
-      <TestContainer>
-        <KeepWorking height={100} />
-      </TestContainer>
-    </PageLayout>
-  );
+  return <RenderDevelopingPage state='Develop' />;
 }
 
 export default addHead(sort, 'closet', '이 페이지는 분류별 차트 페이지입니다');
-
-const TestContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 800px;
-`;
