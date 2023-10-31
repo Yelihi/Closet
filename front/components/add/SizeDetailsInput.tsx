87f20fc338f7907e44861da4550857bec76df691
Yelihi <Yelihi19@gmail.com>
2023-11-01 01:12:13 +0900

    feat: add page -> SizeDetailsInput 추가

--- front/components/add/SizeDetailsInput.tsx
+++ front/components/add/SizeDetailsInput.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
 import { FieldValues, useController } from 'react-hook-form';
 import { SpecificationElementProps } from './data/InputDataInAdd';
 
 import InputBackground from '../recycle/add/InputBackgroud';
 import useControlLibsElements from '../../hooks/useControlLibsElements';
+import { ErrorMessage } from './ItemSpecsInput';
 import { TControl } from '../recycle/element/type';
 import { LibsElementsProps } from '../../hooks/types/libsElementsProps';
 
-type ItemSpecsInputProps<T extends FieldValues, K extends keyof LibsElementsProps> = SpecificationElementProps<K> &
+type SizeDetailsInputProps<T extends FieldValues, K extends keyof LibsElementsProps> = SpecificationElementProps<K> &
   TControl<T> & { textArea?: boolean };
 
-const ItemSpecsInput = <T extends FieldValues, K extends keyof LibsElementsProps>(props: ItemSpecsInputProps<T, K>) => {
+const SizeDetailsInput = <T extends FieldValues, K extends keyof LibsElementsProps>(
+  props: SizeDetailsInputProps<T, K>
+) => {
   const { elementType, subTitle, control, name, errorMessage, elementProps, rules, textArea } = props;
   const {
     field: { value, onChange },
@@ -21,22 +23,11 @@ const ItemSpecsInput = <T extends FieldValues, K extends keyof LibsElementsProps
   const view = useControlLibsElements({ elementType, name, value, onChange, elementProps });
 
   return (
-    <InputBackground title={name} subTitle={subTitle} textArea={textArea}>
+    <InputBackground title={name.split('.')[1]} subTitle={subTitle} textArea={textArea}>
       {view}
       <ErrorMessage>{error && errorMessage}</ErrorMessage>
     </InputBackground>
   );
 };
 
-export default ItemSpecsInput;
-
-export const ErrorMessage = styled.span`
-  display: inline-block;
-  width: 100%;
-  height: 14px;
-  font-size: 11px;
-  margin-top: 0.5rem;
-  font-weight: ${({ theme }) => theme.fontWeight.Medium};
-  font-family: ${({ theme }) => theme.font.Kfont};
-  color: ${({ theme }) => theme.colors.red};
-`;
+export default SizeDetailsInput;
